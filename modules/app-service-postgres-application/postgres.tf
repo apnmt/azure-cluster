afbfe827025b74bfb7badf1279b7236445c484b0
tobi5775 <tobias.wittmann96@web.de>
2022-03-14 14:46:53 +0100

    sort ips to get first and last

--- modules/app-service-postgres-application/postgres.tf
+++ modules/app-service-postgres-application/postgres.tf
@@ -12,11 +12,11 @@ resource "azurerm_postgresql_server" "postgresql-server" {
   storage_mb        = "5120"
   auto_grow_enabled = true
 
-  backup_retention_days        = 7
-  geo_redundant_backup_enabled = false
-  public_network_access_enabled     = true
-  ssl_enforcement_enabled           = true
-  ssl_minimal_tls_version_enforced  = "TLS1_2"
+  backup_retention_days            = 7
+  geo_redundant_backup_enabled     = false
+  public_network_access_enabled    = true
+  ssl_enforcement_enabled          = true
+  ssl_minimal_tls_version_enforced = "TLS1_2"
 }
 
 resource "azurerm_postgresql_database" "postgresql-db" {
@@ -27,11 +27,15 @@ resource "azurerm_postgresql_database" "postgresql-db" {
   collation           = "English_United States.1252"
 }
 
+locals {
+  sorted_ips = sort(azurerm_app_service.appservice.outbound_ip_address_list)
+}
+
 # enable access from all azure services
 resource "azurerm_postgresql_firewall_rule" "firewall" {
   name                = "${var.application_name}-firewall"
   resource_group_name = var.resource_group
   server_name         = azurerm_postgresql_server.postgresql-server.name
-  start_ip_address    = azurerm_app_service.appservice.outbound_ip_address_list.0
-  end_ip_address      = element(azurerm_app_service.appservice.outbound_ip_address_list, length(azurerm_app_service.appservice.outbound_ip_address_list)-1)
+  start_ip_address    = local.sorted_ips.0
+  end_ip_address      = element(local.sorted_ips, length(local.sorted_ips)-1)
 }
