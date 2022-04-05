63e1b3041846f2d55e6fa7db65acaa54ccc30fe5
tobi5775 <tobias.wittmann96@web.de>
2022-04-05 17:58:49 +0200

    change api name and sku

--- api.tf
+++ api.tf
@@ -1,9 +1,14 @@
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
 resource "azurerm_api_management" "apim" {
-  name                = "apnmt-apim"
+  name                = "apnmt-apim-${random_string.suffix.result}"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   publisher_name      = "Apnmt"
   publisher_email     = "apnmt@apnmt.io"
 
-  sku_name = "Developer_1"
+  sku_name = "Standard_1"
 }
