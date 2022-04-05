aeaad4d661979ee806f6eafe5e3187d7dcebb403
tobi5775 <tobias.wittmann96@web.de>
2022-04-05 17:59:53 +0200

    change to multiple firewall rules

--- modules/app-service-postgres-application/postgres.tf
+++ modules/app-service-postgres-application/postgres.tf
@@ -27,15 +27,13 @@ resource "azurerm_postgresql_database" "postgresql-db" {
   collation           = "English_United States.1252"
 }
 
-locals {
-  sorted_ips = sort(azurerm_app_service.appservice.outbound_ip_address_list)
-}
-
 # enable access from all azure services
 resource "azurerm_postgresql_firewall_rule" "firewall" {
-  name                = "${var.application_name}-firewall"
+  count               = length(azurerm_app_service.appservice.outbound_ip_address_list)
+  name                = "${var.application_name}-firewall-${count.index}"
   resource_group_name = var.resource_group
   server_name         = azurerm_postgresql_server.postgresql-server.name
-  start_ip_address    = local.sorted_ips.0
-  end_ip_address      = element(local.sorted_ips, length(local.sorted_ips)-1)
+  start_ip_address    = azurerm_app_service.appservice.outbound_ip_address_list[count.index]
+  end_ip_address      = azurerm_app_service.appservice.outbound_ip_address_list[count.index]
+  depends_on          = [azurerm_app_service.appservice]
 }
